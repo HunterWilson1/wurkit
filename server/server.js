2fed9675af3928b4778b5488f74e36a7143aee33
Hunter Wilson <huntertheguy123@gmail.com>
2023-06-27 17:46:06 -0500

    worked on server and now routes

--- server/server.js
+++ server/server.js
@@ -1,1 +1,17 @@
+require('dotenv').config()
 const express = require('express');
+
+const app = express()
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
+app.get('/', (req, res) => {
+    res.json()
+})
+
+app.listen(process.env.PORT, () => {
+    console.log('listening on port 3000')
+})
