5c485d9b836510d9406bb276665c90d1d0558f74
Hunter Wilson <huntertheguy123@gmail.com>
2023-06-28 08:35:08 -0500

    fixed time

--- client/src/components/Workouts.js
+++ client/src/components/Workouts.js
@@ -1,3 +1,5 @@
+import formatDistanceToNow from "date-fns/formatDistanceToNow";
+
 const Workouts = ({ workout }) => {
     const handleClick = async () => {
       const response = await fetch('./api/workouts/' + workout._id, {
@@ -15,7 +17,7 @@ const Workouts = ({ workout }) => {
         <h4 className="text-primary text-2xl mb-10">{workout.title}</h4>
         <p className="text-sm mb-2">Weight: {workout.weight}</p>
         <p className="text-sm mb-2">Reps: {workout.reps}</p>
-        <p className="text-sm mb-2">{workout.createdAt}</p>
+        <p className="text-sm mb-2">{formatDistanceToNow(new Date(workout.createdAt), { addSuffix: true})}</p>
         <span onClick={handleClick} className="absolute top-6 right-6 cursor-pointer bg-gray-200 p-2 rounded-full text-gray-700">
           &#x2716;
         </span>
